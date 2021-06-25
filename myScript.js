be9aac448d452723a73217044da16469bd1e2f21
nothembah <74840172+nothembah@users.noreply.github.com>
2021-06-25 12:03:42 +0200

    Update myScript.js
    added function either_only

--- myScript.js
+++ myScript.js
@@ -6,4 +6,17 @@ function is_div_by_5(num){
     }
 }
 
-console.log(is_div_by_5(34));
+function either_only(num){
+    if(num % 3 === 0 && num % 5 !== 0){
+        return true;
+    } else if(num % 5 === 0 && num % 3 !== 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let x = 33;
+
+console.log(is_div_by_5(x));
+console.log(either_only(x));
